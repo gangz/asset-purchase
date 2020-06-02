5e74170ec3425de5362890ced8031522ccd262f6
Gang ZHANG <gangz@emergentdesign.cn>
2020-06-03 06:58:06 +0800

    created as a spring-boot project

---
+++ src/test/java/com/github/gangz2009/demo/purchase/PurchaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gangz2009.demo.purchase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
